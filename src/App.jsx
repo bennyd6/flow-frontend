6161d198575ee8fe82958da75c45029b1b562041
bennyd6 <228r1a66d6@cmrec.ac.in>
2025-08-24 11:44:57 +0530

    added dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
 import Landing from './pages/landing'
 import Dashboard from './pages/dashboard'
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
-
   return (
-    <>
-    <Dashboard></Dashboard>
-    <Landing></Landing>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   )
 }
 
